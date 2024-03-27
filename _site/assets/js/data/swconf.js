b6817fc7e3893406d3ca86e38f9d5c0008ad7e27
Yuu <86243871+anzuukino@users.noreply.github.com>
2024-03-28 02:40:34 +0700

    update-PicoCTF2024-Wu-part1

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1711559027',resources: [
+    cacheName: 'chirpy-1711568411',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
