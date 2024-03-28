b3bf79ef2cd6f91044d8a8557cfd79a22858d978
Yuu <86243871+anzuukino@users.noreply.github.com>
2024-03-28 12:19:04 +0700

    big_update: added Background

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1711570624',resources: [
+    cacheName: 'chirpy-1711603101',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
