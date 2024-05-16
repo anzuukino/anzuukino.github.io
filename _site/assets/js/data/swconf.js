45a193d0895d895bb5a10c61ce40241ea34a9826
Yuu <86243871+anzuukino@users.noreply.github.com>
2024-05-16 14:10:34 +0700

    Update KCSCCTF2024_3

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1715843329',resources: [
+    cacheName: 'chirpy-1715843390',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
