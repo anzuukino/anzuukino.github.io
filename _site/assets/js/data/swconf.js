f548506693a5f62262151995d231980b65afcf95
Yuu <86243871+anzuukino@users.noreply.github.com>
2024-06-29 00:28:58 +0700

    Update-Wannagame2024

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1715843549',resources: [
+    cacheName: 'chirpy-1719595491',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
