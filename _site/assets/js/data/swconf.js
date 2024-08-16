10b128af8ad5c196268ab5c1cf0680201a512546
Yuu <86243871+anzuukino@users.noreply.github.com>
2024-08-16 19:00:39 +0700

    update escapevm

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1719596008',resources: [
+    cacheName: 'chirpy-1723809557',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
