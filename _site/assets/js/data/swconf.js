3e422e453a2efffd074b13c7be556a9d714edfe0
Yuu <86243871+anzuukino@users.noreply.github.com>
2024-08-21 00:51:49 +0700

    redacted flag

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1724174614',resources: [
+    cacheName: 'chirpy-1724176210',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
